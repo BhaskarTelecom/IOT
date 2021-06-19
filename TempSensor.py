e143662de9084f90ff09ed08d92c25e04bcad8df
Ashwini Kumar Gupta <kittu1796@gmail.com>
2021-06-19 17:01:40 +0200

    shifting to ubuntu

--- TempSensor.py
+++ TempSensor.py
@@ -1,6 +1,22 @@
-class ClassName(object):
-	"""docstring for ClassName"""
-	def __init__(self, arg):
-		super(ClassName, self).__init__()
-		self.arg = arg
-		
+import numpy as np
+
+
+class TemperatureSensor():
+	"""docstring for TemperatureSensor"""
+	sensorType = "temperature"
+	unit = "celsius"
+
+	def __init__(self, tempSensorType, instanceID):
+		self.instanceID = instanceID
+		self.mean = 90
+		self.variance = 4
+		self.value = 0.0
+
+	def sense(self):
+		# self.value = self.value + self.simpleRandom()
+		self.value = np.random.Generator.normal(self.mean, self.variance)
+
+		return self.value
+
+sensor = TemperatureSensor(1,3)
+print(sensor.sense())
